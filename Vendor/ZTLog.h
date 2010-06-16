5c6194a7243b62487426fe2ec0e5e7e7a9900998
Luke Redpath <luke@lukeredpath.co.uk>
2010-06-16 18:16:27 +0100

    Updated ZTWebSocket to use a git submodule of my own fork on Github.

--- Vendor/ZTLog.h
+++
@@ -1,7 +0,0 @@
-#ifdef DEBUG
-    #define ZTLog(xx, ...) NSLog(@"%s(%d): " xx, ((strrchr(__FILE__, '/') ? : __FILE__- 1) + 1), __LINE__, ##__VA_ARGS__)
-#else
-    #define ZTLog(xx, ...) ((void)0)
-#endif
-
-#define ZTLogRect(r) ZTLog(@"%s x=%f, y=%f, w=%f, h=%f", #r, r.origin.x, r.origin.y, r.size.width, r.size.height)
