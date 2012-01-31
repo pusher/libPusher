9cc916ab8717804a63cae6916f28920a26b36508
Luke Redpath <luke@lukeredpath.co.uk>
2012-01-31 18:17:12 +0000

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
-#!/bin/bash -e
+#!/bin/sh 
+echo "*****************************"
+echo "UPDATING PROJECT DEPENDENCIES "
+echo "*****************************"
+
+echo "* Updating gems for MacRuby"
 source "$HOME/.rvm/scripts/rvm"
+rvm use macruby-nightly > /dev/null
+bundle install > /dev/null
+
+echo "* Updating CocoaPods"
+bundle exec pod install > /dev/null
+echo ""
+
+echo "*****************************"
+echo "PREPARING FOR BUILD"
+echo "*****************************"
+
+echo "* Updating gems for MRI"
 rvm use default
 bundle install
-rake
+echo ""
+
+bundle exec rake debug:build
+
