b13fdd09248877a5f73a483e7a1491645bb75974
Luke Redpath <luke@lukeredpath.co.uk>
2012-02-03 08:47:58 +0000

    The OSX build does need CocoaPods, even though it links to the source files directly.

--- Scripts/build-osx-nightly.sh
+++ Scripts/build-osx-nightly.sh
@@ -1,9 +1,20 @@
 #!/bin/sh 
 echo "*****************************"
-echo "PREPARING FOR BUILD"
+echo "UPDATING PROJECT DEPENDENCIES "
 echo "*****************************"
 
+echo "* Updating gems for MacRuby"
 source "$HOME/.rvm/scripts/rvm"
+rvm use macruby-nightly > /dev/null
+bundle install --without building scripts > /dev/null
+
+echo "* Updating CocoaPods"
+bundle exec pod install > /dev/null
+echo ""
+
+echo "*****************************"
+echo "PREPARING FOR BUILD"
+echo "*****************************"
 
 echo "* Updating gems for MRI"
 rvm use default
