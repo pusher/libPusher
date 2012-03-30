a2a438feb1ca10dd4319410510aa4957b9da9736
Luke Redpath <luke@lukeredpath.co.uk>
2012-03-30 12:29:02 +0100

    I still need the JSONKit header file to compile.

---
+++ Library/JSONKit.h
@@ -0,0 +1,14 @@
+//
+//  JSONKit.h
+//  libPusher
+//
+//  Created by Luke Redpath on 30/03/2012.
+//  Copyright (c) 2012 LJR Software Limited. All rights reserved.
+//
+
+#ifndef libPusher_JSONKit_h
+#define libPusher_JSONKit_h
+
+
+
+#endif
