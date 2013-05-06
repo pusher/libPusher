dbbee466f0d34a76eb3b655f8c0ed306dd59658c
Luke Redpath <luke@lukeredpath.co.uk>
2013-05-06 14:26:20 +0100

    We can remove this authorisation bypass code now that we can just use delegation.

---
+++ Functional Specs/PTPusherChannelAuthorizationBypass.h
@@ -0,0 +1,14 @@
+//
+//  PTPusherChannelAuthorizationBypass.h
+//  libPusher
+//
+//  Created by Luke Redpath on 06/05/2013.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "PTPusherChannelAuthorizationDelegate.h"
+
+@interface PTPusherChannelAuthorizationBypass : NSObject <PTPusherChannelAuthorizationDelegate>
+
+@end
