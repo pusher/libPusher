90d3d4b279d4913397b1f01e0a80a693a9a37729
Brian Croom and Ivan Rodriguez <pair+bcroom+irodriguez@pivotal.io>
2016-04-14 12:45:04 -0400

    Import SocketRocket using the angle-bracket syntax

--- Library/PTPusherConnection.m
+++ Library/PTPusherConnection.m
@@ -9,7 +9,7 @@
 #import "PTPusherConnection.h"
 #import "PTPusherEvent.h"
 #define SR_ENABLE_LOG
-#import "SRWebSocket.h"
+#import <SocketRocket/SRWebSocket.h>
 #import "PTJSON.h"
 
 NSString *const PTPusherConnectionEstablishedEvent = @"pusher:connection_established";
