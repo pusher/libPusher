aa62b7d27e785a6885dc6401f7ea15cd3785f68b
Hamilton Chapman <hamchapman@gmail.com>
2016-06-16 13:07:13 +0100

    Fully fixed libPusher-OSX setup to work with sample app as well

---
+++ libPusher-OSX/PusherSampleOSX/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  PusherSampleOSX
+//
+//  Created by Hamilton Chapman on 16/06/2016.
+//  Copyright © 2016 LJR Software Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
