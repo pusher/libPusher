c69129b245577c50643f11430b65e9ce9020611d
Hamilton Chapman <hamchapman@gmail.com>
2016-06-29 14:17:48 +0100

    Add some logging for debugging ATS exception

--- Library/PTURLRequestOperation.m
+++ Library/PTURLRequestOperation.m
@@ -89,6 +89,7 @@
 
 - (void)URLSession:(NSURLSession *)session dataTask:(NSURLSessionDataTask *)dataTask didReceiveData:(NSData *)data
 {
+  NSLog(@"data: %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
   if (self.responseData == nil) { // this might be called before didReceiveResponse
     self.responseData = [NSMutableData data];
   }
@@ -100,6 +101,7 @@
 
 - (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
 {
+  NSLog(@"error: %@", error.localizedDescription);
   self.connectionError = error;
   [self finish];
 }
