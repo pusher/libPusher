2607065e2298b927af8de0f59780c8e288b63b32
Oliver Pfeffer <oli.pfeffer@ring.com>
2020-01-06 11:58:06 -0500

    Fix header imports of SocketRocket.h

--- Library/PTPusher.m
+++ Library/PTPusher.m
@@ -16,7 +16,7 @@
 #import "PTPusherErrors.h"
 #import "PTPusherChannelServerBasedAuthorization.h"
 #import "PTPusherChannel_Private.h"
-#import "SRWebSocket.h"
+#import <SocketRocket/SRWebSocket.h>
 
 #define kPUSHER_HOST @"ws.pusherapp.com"
 
